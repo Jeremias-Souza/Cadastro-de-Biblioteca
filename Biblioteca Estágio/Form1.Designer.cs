1825320fa1448dee204009782d5adab132ff8f7a
estagiario1 <estagiario1@DESKTOP-LMGKQ6A>
2022-09-20 11:52:48 -0300

    Adicionar arquivos de projeto.

---
+++ Biblioteca Estágio/Form1.Designer.cs
@@ -0,0 +1,188 @@
+﻿namespace Biblioteca_Estágio
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.CodAutor = new System.Windows.Forms.TextBox();
+            this.NomeAutor = new System.Windows.Forms.TextBox();
+            this.InfAutor = new System.Windows.Forms.TextBox();
+            this.btnSave = new System.Windows.Forms.Button();
+            this.btnDelete = new System.Windows.Forms.Button();
+            this.btnLoadImage = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft YaHei", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.SystemColors.Highlight;
+            this.label1.Location = new System.Drawing.Point(226, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(204, 28);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Cadastro do autor";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft JhengHei UI", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(23, 68);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(108, 17);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Código do autor";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft JhengHei UI", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(23, 104);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(111, 17);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Nome completo ";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft JhengHei UI", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(23, 139);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(142, 17);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "Informações do Autor";
+            // 
+            // CodAutor
+            // 
+            this.CodAutor.Location = new System.Drawing.Point(146, 65);
+            this.CodAutor.Name = "CodAutor";
+            this.CodAutor.Size = new System.Drawing.Size(365, 20);
+            this.CodAutor.TabIndex = 4;
+            this.CodAutor.TextChanged += new System.EventHandler(this.CodAutor_TextChanged);
+            // 
+            // NomeAutor
+            // 
+            this.NomeAutor.Location = new System.Drawing.Point(146, 101);
+            this.NomeAutor.Name = "NomeAutor";
+            this.NomeAutor.Size = new System.Drawing.Size(365, 20);
+            this.NomeAutor.TabIndex = 5;
+            this.NomeAutor.TextChanged += new System.EventHandler(this.NomeAutor_TextChanged);
+            // 
+            // InfAutor
+            // 
+            this.InfAutor.Location = new System.Drawing.Point(171, 139);
+            this.InfAutor.Name = "InfAutor";
+            this.InfAutor.Size = new System.Drawing.Size(340, 20);
+            this.InfAutor.TabIndex = 6;
+            this.InfAutor.TextChanged += new System.EventHandler(this.InfAutor_TextChanged);
+            // 
+            // btnSave
+            // 
+            this.btnSave.Location = new System.Drawing.Point(194, 195);
+            this.btnSave.Name = "btnSave";
+            this.btnSave.Size = new System.Drawing.Size(128, 31);
+            this.btnSave.TabIndex = 9;
+            this.btnSave.Text = "Salvar";
+            this.btnSave.UseVisualStyleBackColor = true;
+            this.btnSave.Click += new System.EventHandler(this.btnSave_Click);
+            // 
+            // btnDelete
+            // 
+            this.btnDelete.Location = new System.Drawing.Point(339, 195);
+            this.btnDelete.Name = "btnDelete";
+            this.btnDelete.Size = new System.Drawing.Size(128, 31);
+            this.btnDelete.TabIndex = 10;
+            this.btnDelete.Text = "Excluir";
+            this.btnDelete.UseVisualStyleBackColor = true;
+            this.btnDelete.Click += new System.EventHandler(this.btnDelete_Click);
+            // 
+            // btnLoadImage
+            // 
+            this.btnLoadImage.Location = new System.Drawing.Point(602, 165);
+            this.btnLoadImage.Name = "btnLoadImage";
+            this.btnLoadImage.Size = new System.Drawing.Size(98, 23);
+            this.btnLoadImage.TabIndex = 12;
+            this.btnLoadImage.Text = "Carregar Imagem";
+            this.btnLoadImage.UseVisualStyleBackColor = true;
+            this.btnLoadImage.Click += new System.EventHandler(this.btnLoadImage_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(589, 58);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(121, 101);
+            this.pictureBox1.TabIndex = 11;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 314);
+            this.Controls.Add(this.btnLoadImage);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.btnDelete);
+            this.Controls.Add(this.btnSave);
+            this.Controls.Add(this.InfAutor);
+            this.Controls.Add(this.NomeAutor);
+            this.Controls.Add(this.CodAutor);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox CodAutor;
+        private System.Windows.Forms.TextBox NomeAutor;
+        private System.Windows.Forms.TextBox InfAutor;
+        private System.Windows.Forms.Button btnSave;
+        private System.Windows.Forms.Button btnDelete;
+        private System.Windows.Forms.Button btnLoadImage;
+        private System.Windows.Forms.PictureBox pictureBox1;
+    }
+}
+
---
+++ Biblioteca Estágio/Form1.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Biblioteca_Estágio
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnSave_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnDelete_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnLoadImage_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                string connetionString;
+                SqlConnection cnn;
+                connetionString = @"Data Source=DESKTOP-LMGKQ6A\TREINAMENTO;Initial Catalog=TREINAMENTO;User ID=Jeremias Souza;Password=MOVTECH2022";
+                cnn = new SqlConnection(connetionString);
+                cnn.Open();
+                MessageBox.Show("Connection Open !");
+                cnn.Close();
+            }
+            catch (SqlException erro)
+            {
+                MessageBox.Show("Erro ao se conectar no banco de dados \n" +
+                "Verifique os dados informados" + erro);
+                throw;
+            }
+
+           /* if(OpenFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                string nome = OpenFileDialog.FileName;
+
+                bmp = new Bitmap(nome)
+            }*/
+
+        }
+
+        private void CodAutor_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void NomeAutor_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void InfAutor_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
