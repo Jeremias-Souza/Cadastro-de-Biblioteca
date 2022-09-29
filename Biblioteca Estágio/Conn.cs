6b1d9c9550765a9b02bcf34236926070f756ca61
estagiario1 <estagiario1@DESKTOP-LMGKQ6A>
2022-09-29 11:35:52 -0300

    Atualização do cadastro de autores para sistema de Biblioteca

---
+++ Biblioteca Estágio/Conn.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing.Text;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web;
+using System.Windows.Forms;
+
+namespace Biblioteca_Estágio
+{
+    internal class Conn
+    {
+        private static string server = @"DESKTOP-LMGKQ6A\TREINAMENTO";
+        private static string dataBase = "CADASTRO_AUTOR";        
+        private static string user = "Jeremias Souza";
+        private static string password = "MOVTECH2022";
+        
+
+        public static string Strcon
+        {
+            get {return $"Data Source= {server}; Integrated Security=False; Initial Catalog= {dataBase};User ID={user}; Password={password}";}
+        }
+     
+    }
+}
+
+ 
--- Biblioteca Estágio/Form1.Designer.cs
+++ Biblioteca Estágio/Form1.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace Biblioteca_Estágio
 {
-    partial class Form1
+    partial class CadastroAutor
     {
         /// <summary>
         /// Variável de designer necessária.
@@ -28,36 +28,33 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.label1 = new System.Windows.Forms.Label();
+            this.components = new System.ComponentModel.Container();
             this.label2 = new System.Windows.Forms.Label();
             this.label3 = new System.Windows.Forms.Label();
             this.label4 = new System.Windows.Forms.Label();
             this.CodAutor = new System.Windows.Forms.TextBox();
             this.NomeAutor = new System.Windows.Forms.TextBox();
             this.InfAutor = new System.Windows.Forms.TextBox();
-            this.btnSave = new System.Windows.Forms.Button();
             this.btnDelete = new System.Windows.Forms.Button();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.statusStrip1 = new System.Windows.Forms.StatusStrip();
+            this.toolStripStatusLabel1 = new System.Windows.Forms.ToolStripStatusLabel();
+            this.AddAutor = new System.Windows.Forms.Button();
+            this.openFileDialog3 = new System.Windows.Forms.OpenFileDialog();
             this.btnLoadImage = new System.Windows.Forms.Button();
+            this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
             this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.statusStrip1.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
             // 
-            // label1
-            // 
-            this.label1.AutoSize = true;
-            this.label1.Font = new System.Drawing.Font("Microsoft YaHei", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.ForeColor = System.Drawing.SystemColors.Highlight;
-            this.label1.Location = new System.Drawing.Point(226, 9);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(204, 28);
-            this.label1.TabIndex = 0;
-            this.label1.Text = "Cadastro do autor";
-            // 
             // label2
             // 
+            this.label2.Anchor = System.Windows.Forms.AnchorStyles.None;
             this.label2.AutoSize = true;
-            this.label2.Font = new System.Drawing.Font("Microsoft JhengHei UI", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label2.Location = new System.Drawing.Point(23, 68);
+            this.label2.Font = new System.Drawing.Font("Microsoft JhengHei UI", 9.75F, System.Drawing.FontStyle.Bold);
+            this.label2.Location = new System.Drawing.Point(17, 16);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(108, 17);
             this.label2.TabIndex = 1;
@@ -65,105 +62,189 @@
             // 
             // label3
             // 
+            this.label3.Anchor = System.Windows.Forms.AnchorStyles.None;
             this.label3.AutoSize = true;
-            this.label3.Font = new System.Drawing.Font("Microsoft JhengHei UI", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label3.Location = new System.Drawing.Point(23, 104);
+            this.label3.Font = new System.Drawing.Font("Microsoft JhengHei UI", 9.75F, System.Drawing.FontStyle.Bold);
+            this.label3.Location = new System.Drawing.Point(17, 45);
             this.label3.Name = "label3";
             this.label3.Size = new System.Drawing.Size(111, 17);
             this.label3.TabIndex = 2;
             this.label3.Text = "Nome completo ";
+            this.label3.Click += new System.EventHandler(this.label3_Click);
             // 
             // label4
             // 
+            this.label4.Anchor = System.Windows.Forms.AnchorStyles.None;
             this.label4.AutoSize = true;
-            this.label4.Font = new System.Drawing.Font("Microsoft JhengHei UI", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label4.Location = new System.Drawing.Point(23, 139);
+            this.label4.Font = new System.Drawing.Font("Microsoft JhengHei UI", 9.75F, System.Drawing.FontStyle.Bold);
+            this.label4.Location = new System.Drawing.Point(17, 76);
             this.label4.Name = "label4";
             this.label4.Size = new System.Drawing.Size(142, 17);
             this.label4.TabIndex = 3;
             this.label4.Text = "Informações do Autor";
+            this.label4.Click += new System.EventHandler(this.label4_Click);
             // 
             // CodAutor
             // 
-            this.CodAutor.Location = new System.Drawing.Point(146, 65);
+            this.CodAutor.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.CodAutor.AutoCompleteMode = System.Windows.Forms.AutoCompleteMode.Append;
+            this.CodAutor.Location = new System.Drawing.Point(161, 12);
+            this.CodAutor.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.CodAutor.Name = "CodAutor";
-            this.CodAutor.Size = new System.Drawing.Size(365, 20);
+            this.CodAutor.Size = new System.Drawing.Size(426, 23);
             this.CodAutor.TabIndex = 4;
             this.CodAutor.TextChanged += new System.EventHandler(this.CodAutor_TextChanged);
+            this.CodAutor.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.CodAutor_KeyPress);
             // 
             // NomeAutor
             // 
-            this.NomeAutor.Location = new System.Drawing.Point(146, 101);
+            this.NomeAutor.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.NomeAutor.Location = new System.Drawing.Point(161, 43);
+            this.NomeAutor.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.NomeAutor.MaxLength = 50;
             this.NomeAutor.Name = "NomeAutor";
-            this.NomeAutor.Size = new System.Drawing.Size(365, 20);
+            this.NomeAutor.Size = new System.Drawing.Size(426, 23);
             this.NomeAutor.TabIndex = 5;
             this.NomeAutor.TextChanged += new System.EventHandler(this.NomeAutor_TextChanged);
+            this.NomeAutor.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.NomeAutor_KeyPress);
             // 
             // InfAutor
             // 
-            this.InfAutor.Location = new System.Drawing.Point(171, 139);
+            this.InfAutor.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.InfAutor.Location = new System.Drawing.Point(161, 74);
+            this.InfAutor.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.InfAutor.MaxLength = 1000;
+            this.InfAutor.Multiline = true;
             this.InfAutor.Name = "InfAutor";
-            this.InfAutor.Size = new System.Drawing.Size(340, 20);
+            this.InfAutor.Size = new System.Drawing.Size(425, 35);
             this.InfAutor.TabIndex = 6;
             this.InfAutor.TextChanged += new System.EventHandler(this.InfAutor_TextChanged);
             // 
-            // btnSave
-            // 
-            this.btnSave.Location = new System.Drawing.Point(194, 195);
-            this.btnSave.Name = "btnSave";
-            this.btnSave.Size = new System.Drawing.Size(128, 31);
-            this.btnSave.TabIndex = 9;
-            this.btnSave.Text = "Salvar";
-            this.btnSave.UseVisualStyleBackColor = true;
-            this.btnSave.Click += new System.EventHandler(this.btnSave_Click);
-            // 
             // btnDelete
             // 
-            this.btnDelete.Location = new System.Drawing.Point(339, 195);
+            this.btnDelete.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.btnDelete.Location = new System.Drawing.Point(393, 118);
+            this.btnDelete.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.btnDelete.Name = "btnDelete";
-            this.btnDelete.Size = new System.Drawing.Size(128, 31);
+            this.btnDelete.Size = new System.Drawing.Size(105, 32);
             this.btnDelete.TabIndex = 10;
             this.btnDelete.Text = "Excluir";
             this.btnDelete.UseVisualStyleBackColor = true;
             this.btnDelete.Click += new System.EventHandler(this.btnDelete_Click);
             // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AccessibleDescription = "NOT NULL";
+            this.dataGridView1.AllowUserToAddRows = false;
+            this.dataGridView1.AllowUserToDeleteRows = false;
+            this.dataGridView1.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.dataGridView1.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.AllCells;
+            this.dataGridView1.BackgroundColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(20, 158);
+            this.dataGridView1.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.ReadOnly = true;
+            this.dataGridView1.Size = new System.Drawing.Size(696, 132);
+            this.dataGridView1.TabIndex = 14;
+            this.dataGridView1.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellContentClick);
+            this.dataGridView1.CellContentDoubleClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellContentDoubleClick);
+            // 
+            // statusStrip1
+            // 
+            this.statusStrip1.BackColor = System.Drawing.SystemColors.InactiveBorder;
+            this.statusStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripStatusLabel1});
+            this.statusStrip1.Location = new System.Drawing.Point(0, 309);
+            this.statusStrip1.Name = "statusStrip1";
+            this.statusStrip1.Padding = new System.Windows.Forms.Padding(2, 0, 16, 0);
+            this.statusStrip1.Size = new System.Drawing.Size(732, 22);
+            this.statusStrip1.TabIndex = 15;
+            this.statusStrip1.Text = "statusStrip1";
+            this.statusStrip1.ItemClicked += new System.Windows.Forms.ToolStripItemClickedEventHandler(this.statusStrip1_ItemClicked);
+            // 
+            // toolStripStatusLabel1
+            // 
+            this.toolStripStatusLabel1.Name = "toolStripStatusLabel1";
+            this.toolStripStatusLabel1.Size = new System.Drawing.Size(118, 17);
+            this.toolStripStatusLabel1.Text = "toolStripStatusLabel1";
+            this.toolStripStatusLabel1.Click += new System.EventHandler(this.toolStripStatusLabel1_Click);
+            // 
+            // AddAutor
+            // 
+            this.AddAutor.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.AddAutor.Location = new System.Drawing.Point(265, 117);
+            this.AddAutor.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.AddAutor.Name = "AddAutor";
+            this.AddAutor.Size = new System.Drawing.Size(105, 32);
+            this.AddAutor.TabIndex = 18;
+            this.AddAutor.Text = "Adicionar";
+            this.AddAutor.UseVisualStyleBackColor = true;
+            this.AddAutor.Click += new System.EventHandler(this.AddAutor_Click);
+            // 
+            // openFileDialog3
+            // 
+            this.openFileDialog3.FileName = "openFileDialog3";
+            this.openFileDialog3.Filter = "JPG(*.jpg)|*.jpg|PNG(*.png)|*png";
+            this.openFileDialog3.FileOk += new System.ComponentModel.CancelEventHandler(this.openFileDialog3_FileOk);
+            // 
             // btnLoadImage
             // 
-            this.btnLoadImage.Location = new System.Drawing.Point(602, 165);
+            this.btnLoadImage.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.btnLoadImage.Location = new System.Drawing.Point(599, 118);
+            this.btnLoadImage.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.btnLoadImage.Name = "btnLoadImage";
-            this.btnLoadImage.Size = new System.Drawing.Size(98, 23);
-            this.btnLoadImage.TabIndex = 12;
+            this.btnLoadImage.Size = new System.Drawing.Size(117, 32);
+            this.btnLoadImage.TabIndex = 20;
             this.btnLoadImage.Text = "Carregar Imagem";
             this.btnLoadImage.UseVisualStyleBackColor = true;
             this.btnLoadImage.Click += new System.EventHandler(this.btnLoadImage_Click);
             // 
+            // contextMenuStrip1
+            // 
+            this.contextMenuStrip1.Name = "contextMenuStrip1";
+            this.contextMenuStrip1.Size = new System.Drawing.Size(61, 4);
+            // 
             // pictureBox1
             // 
-            this.pictureBox1.Location = new System.Drawing.Point(589, 58);
+            this.pictureBox1.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.pictureBox1.BackColor = System.Drawing.SystemColors.ButtonFace;
+            this.pictureBox1.Image = global::Biblioteca_Estágio.Properties.Resources._11;
+            this.pictureBox1.Location = new System.Drawing.Point(599, 11);
+            this.pictureBox1.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(121, 101);
+            this.pictureBox1.Size = new System.Drawing.Size(117, 98);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
             this.pictureBox1.TabIndex = 11;
             this.pictureBox1.TabStop = false;
             this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click);
             // 
-            // Form1
+            // CadastroAutor
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 314);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.None;
+            this.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.ClientSize = new System.Drawing.Size(732, 331);
             this.Controls.Add(this.btnLoadImage);
+            this.Controls.Add(this.AddAutor);
+            this.Controls.Add(this.statusStrip1);
+            this.Controls.Add(this.dataGridView1);
             this.Controls.Add(this.pictureBox1);
             this.Controls.Add(this.btnDelete);
-            this.Controls.Add(this.btnSave);
             this.Controls.Add(this.InfAutor);
             this.Controls.Add(this.NomeAutor);
             this.Controls.Add(this.CodAutor);
             this.Controls.Add(this.label4);
             this.Controls.Add(this.label3);
             this.Controls.Add(this.label2);
-            this.Controls.Add(this.label1);
-            this.Name = "Form1";
-            this.Text = "Form1";
+            this.Font = new System.Drawing.Font("Microsoft Tai Le", 9F);
+            this.ForeColor = System.Drawing.Color.Black;
+            this.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.Name = "CadastroAutor";
+            this.Text = "Cadastro do Autor";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.statusStrip1.ResumeLayout(false);
+            this.statusStrip1.PerformLayout();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
@@ -171,18 +252,21 @@
         }
 
         #endregion
-
-        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ToolStripStatusLabel toolStripStatusLabel1;
+        private System.Windows.Forms.OpenFileDialog openFileDialog3;
         private System.Windows.Forms.Label label2;
         private System.Windows.Forms.Label label3;
         private System.Windows.Forms.Label label4;
-        private System.Windows.Forms.TextBox CodAutor;
         private System.Windows.Forms.TextBox NomeAutor;
         private System.Windows.Forms.TextBox InfAutor;
-        private System.Windows.Forms.Button btnSave;
         private System.Windows.Forms.Button btnDelete;
-        private System.Windows.Forms.Button btnLoadImage;
         private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.StatusStrip statusStrip1;
+        private System.Windows.Forms.Button AddAutor;
+        private System.Windows.Forms.Button btnLoadImage;
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
+        private System.Windows.Forms.TextBox CodAutor;
     }
 }
 
